237d9f97527a23355cd0bd083439a7b586be526b
mbercx <marnik.bercx@uantwerpen.be>
2020-01-21 11:38:02 +0100

    add: LaTeX compile script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pdflatex -output-dir=output '\PassOptionsToPackage{draft}{graphicx}\input{main}'
+rsync -av bibliography/ output/bibliography
 cd output
 bibtex main1-blx
 bibtex main2-blx
@@ -13,3 +14,4 @@ cd ..
 pdflatex -output-dir=output '\PassOptionsToPackage{draft}{graphicx}\input{main}'
 pdflatex -output-dir=output '\PassOptionsToPackage{draft}{graphicx}\input{main}'
 pdflatex -output-directory=output main
+cp output/main.pdf ./thesis.pdf
